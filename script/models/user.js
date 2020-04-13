ce79d9cab68c0e60fbb9110c7628d75994837ccc
chefkasperson <chefkasperson@gmail.com>
2020-04-13 13:41:09 -0700

    added divs

---
+++ script/models/user.js
@@ -0,0 +1,22 @@
+
+class User {
+  static all = []
+  constructor(data){
+    this.id = data.id
+    this.name = data.name
+    this.save()
+  }
+  
+
+  save() {
+    User.all.push(this)
+  }
+
+  renderLogInForm() {
+
+  }
+
+  renderNewUserForm() {
+    
+  }
+}
