097952d5b02ea05f237bda0181f5520621cfd4c7
chefkasperson <chefkasperson@gmail.com>
2020-04-15 06:59:09 -0700

    added dom stuff

--- script/services/dom.js
+++ script/services/dom.js
@@ -1,3 +1,30 @@
 class DOM {
-  
+  static resetNav() {
+    const navDiv = document.querySelector('#nav')
+    navDiv.innerHTML = Navbar.render()
+  }
+
+  static loadLoginForm() {
+    const main = document.querySelector('#main-container')
+    main.innerHTML = Auth.renderLoginForm()
+  }
+
+  static loadSignupForm() {
+    const main = document.querySelector('#main-container')
+    main.innerHTML = User.renderSignupForm()
+  }
+
+  static loadMainContainer() {
+    this.resetNav()
+    if (Auth.isSignedIn) {
+      this.loadUserDashboard()
+    } else {
+      this.loadLoginForm()
+    }
+  }
+
+  static loadUserDashboard() {
+    const main = document.querySelector('#main-container')
+    main.innerHTML = `<h1> Display some rilly coo stuff </h1>`
+  }
 }
